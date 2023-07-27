7236e1a3b04d284659eb908e46f8472fc7d9807b
Max Patiiuk <max@patii.uk>
2023-07-26 20:15:38 -0700

    feat: Add an easy way to create a text editor

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ const imgDir = 'imgs/';
 const imgCount = 30;
 const imgExt = '.jpg';
 
-const imgIndex = Math.floor(Math.random() * imgCount)+1;
+const imgIndex = Math.floor(Math.random() * imgCount) + 1;
 const imgSrc = `${imgDir}${imgIndex}${imgExt}`;
 
 const img = document.getElementById('img');
 img.setAttribute('src', imgSrc);
 
+const controller = new AbortController();
+
+img.addEventListener(
+  'dblclick',
+  () => {
+    document.body.innerHTML += `<textarea class="textarea" autofocus></textarea>`;
+  },
+  { abort: controller.signal },
+);
